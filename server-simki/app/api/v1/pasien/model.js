ef25d3ebf794db5f41fbc081363968bc2299e839
aldyn aditya <aldynaditya@gmail.com>
2024-05-13 16:13:56 +0700

    Second Commit

---
+++ server-simki/app/api/v1/pasien/model.js
@@ -0,0 +1,15 @@
+// const db = require('./app/config');
+// const sequelize = require('sequelize'); 
+
+// const { DataTypes } = ;
+
+// const Pasien = db.define('pasien', {
+//     nama: {
+//         type: DataTypes.STRING,
+//         allowNull: false
+//     }
+// }, {
+//     timestamps: true
+// });
+
+module.exports = Pasien;
